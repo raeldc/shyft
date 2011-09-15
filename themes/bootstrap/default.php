3fe25d9056864486d057c834f003a10bb6c73e28
Israel Canasa <raeldc@gmail.com>
2011-09-16 02:00:26 +0800

    Updated theme for showing sample results.

--- themes/bootstrap/default.php
+++ themes/bootstrap/default.php
@@ -1,67 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title><?=@container('title')?></title>
-    <?=@container('meta')?>
-    <?=@container('scripts')?>
-    <?=@container('styles')?>
+<head>
+<title><?=@container('title')?></title>
+<?=@container('meta')?>
+<?=@container('scripts')?>
+<?=@container('styles')?>
 
-    <!-- HTML5 shim, for IE6-8 support of HTML elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+<!-- HTML5 shim, for IE6-8 support of HTML elements -->
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
 
-    <link href="css://bootstrap-1.2.0.css" rel="stylesheet">
-  </head>
+<link href="css://bootstrap-1.2.0.css" rel="stylesheet">
+<link href="css://theme.css" rel="stylesheet">
+</head>
 
-  <body>
+<body>
 
-    <div class="topbar">
-      <div class="fill">
-        <div class="container">
-          <h3><a href="#">Project name</a></h3>
-          <ul class="nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="index.php?com=nodes&view=nodes&layout=admin">Nodes</a></li>
-            <li><a href="index.php?com=nodes&view=nodes">List</a></li>
-          </ul>
+    <header class="topbar">
+        <div class="fill">
+            <div class="container">
+                <h3><a href="#">Flowku Project</a></h3>
+
+                <nav>
+                    <ul class="nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">The Tutorial</a></li>
+                        <li><a href="#">About Me</a></li>
+                    </ul>
+                </nav>
+
+            </div>
         </div>
-      </div>
-    </div>
+    </header>
 
     <div class="container">
 
-      <!-- Main hero unit for a primary marketing message or call to action -->
-      <div class="hero-unit">
-        <h1>FLowku - Content Workflow System</h1>
-        <p><?=@container('page')?></p>
-        <p><a class="btn primary large">Learn more &raquo;</a></p>
-      </div>
+        <!-- Main hero unit for a primary marketing message or call to action -->
+        <article class="hero-unit">
+            <h1>Learn Nooku in Small Easy Steps</h1>
+            <p></p>
+            <p><a class="btn primary large">Learn more &raquo;</a></p>
+        </article>
 
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="span6">
-          <h2>Heading</h2>
-          <p>Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
+        <div class="row">
+            <div class="span4 columns">
+                <a href="#" class="rounded highlight gray main btn-huge active">
+                    <h2>Chapter 1</h2>
+                    <p>Do it first, understand later</p>
+                </a>
+                
+                <nav class="highlight lightblue rounded list">
+                    <a class="underlined" href="index.php?com=content&view=content&layout=form">Add a Page</a>
+                    <a class="underlined" href="index.php?com=content&view=contents">List of Contents</a><br />
+                </nav>
+            </div>
+
+            <section class="span8 columns">
+                <?=@container('page')?>
+            </section>
         </div>
-        <div class="span5">
-          <h2>Heading</h2>
-           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-       </div>
-        <div class="span5">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div>
-      </div>
 
-      <footer>
-        <p>&copy; Company 2011</p>
-      </footer>
 
-    </div> <!-- /container -->
+        <footer>
+            <p>&copy; 2011 Israel D. Canasa</p>
+        </footer>
+    </div><!--/container-->
 
-  </body>
+</body>
 </html>
