c310a326853bdc826be0c9c13d6cb49f616114de
Israel Canasa <raeldc@gmail.com>
2011-09-15 14:09:01 +0800

    Dispatcher now dispatches component's dispatcher.
    Default component is "contents"

--- components/application/dispatcher.php
+++ components/application/dispatcher.php
@@ -18,6 +18,8 @@ class ComApplicationDispatcher extends KControllerAbstract implements KObjectIns
         }
 
         $this->registerCallback('after.dispatch', array($this, 'render'));
+
+        $this->_component = $config->component;
     }
     
 	protected function _initialize(KConfig $config)
@@ -26,6 +28,7 @@ class ComApplicationDispatcher extends KControllerAbstract implements KObjectIns
     		'request'				=> KRequest::get('get', 'string'),
     		// TODO: Behavior should be configurable in database
     		'behaviors'				=> array('routable'),
+            'component'             => KRequest::get('get.com', 'cmd', 'contents'),
         ))->append(array(
             'request' 				=> array('format' => KRequest::format() ? KRequest::format() : 'html')
         ));
@@ -54,7 +57,7 @@ class ComApplicationDispatcher extends KControllerAbstract implements KObjectIns
 	
 	protected function _actionDispatch(KCommandContext $context)
 	{
-       return 'Solid Framework + Fast and Scalable Database + Content Workflow Management = The Web Developer&rsquo;s Dream!';
+        return $this->getComponent()->execute('dispatch', $context);
 	}
 
     protected function _actionForward(KCommandContext $context)
@@ -88,5 +91,63 @@ class ComApplicationDispatcher extends KControllerAbstract implements KObjectIns
             }
             else return $context->result;
         }
+    }
+
+    /**
+     * Method to get a Component Dispatcher object
+     *
+     * @return  KDispatcherAbstract
+     */
+    public function getComponent()
+    {
+        if(!($this->_component instanceof KDispatcherAbstract))
+        {  
+            //Make sure we have a dispatcher identifier
+            if(!($this->_component instanceof KIdentifier)) {
+                $this->setComponent($this->_component);
+            }
+
+            $this->_component = KFactory::get($this->_component);
+        }
+    
+        return $this->_component;
+    }
+
+    /**
+     * Method to set a component dispatcher object attached to the application dispatcher
+     *
+     * @param   mixed   An object that implements KObjectIdentifiable, an object that
+     *                  implements KIdentifierInterface or valid identifier string
+     * @throws  KDispatcherException    If the identifier is not a dispatcher identifier
+     * @return  KDispatcherAbstract
+     */
+    public function setComponent($component)
+    {
+        if(!($component instanceof KDispatcherAbstract))
+        {
+            if(is_string($component) && strpos($component, '.') === false ) 
+            {
+                // Dispatcher names are always singular
+                if(KInflector::isPlural($component)) {
+                    $component = KInflector::singularize($component);
+                } 
+                
+                $identifier             = clone $this->_identifier;
+                $identifier->package    = $this->_component;
+                $identifier->path       = array();
+                $identifier->name       = 'dispatcher';
+            }
+            else $identifier = KIdentifier::identify($component);
+
+            if($identifier->name != 'dispatcher') {
+                throw new KDispatcherException('Identifier: '.$identifier.' is not a component dispatcher identifier');
+            }
+
+            $component = $identifier;
+        }
+        
+        $this->_component = $component;
+    
+        return $this;
     }
 }
---
+++ components/contents/views/contents/html.php
@@ -0,0 +1,10 @@
+<?php
+
+class ComContentsViewContentsHtml extends KViewHtml
+{
+	public function display()
+	{
+		//return "hi";
+		return parent::display();
+	}
+}
