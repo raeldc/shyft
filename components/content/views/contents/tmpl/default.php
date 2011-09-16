5cea12d227fb3bfe11d034a1fb80abefb7d2d788
Israel Canasa <raeldc@gmail.com>
2011-09-16 13:03:10 +0800

    Nicer Table layout for the list

--- components/content/views/contents/tmpl/default.php
+++ components/content/views/contents/tmpl/default.php
@@ -1,3 +1,21 @@
-<?foreach ($contents as $content):?>
-	<a href="index.php?com=content&id=<?=$content->_id;?>&layout=form"><?=$content->title;?></a><br />
-<?endforeach;?>
+<form method="get" action="<?=@route()?>" class="-koowa-grid">
+<table class="zebra-striped" id="sortTableExample">
+	<thead>
+		<tr>
+			<th class="header">#</th>
+			<th class="yellow header">Title</th>
+			<th class="blue header">Body</th>
+		</tr>
+	</thead>
+
+	<tbody>
+		<?php foreach ($contents as $content): ?>
+		<tr>
+			<td><?=@helper('grid.checkbox',array('row' => $content))?></td>
+			<td><a href="index.php?com=content&id=<?=$content->id;?>&layout=form"><?=$content->title;?></a></td>
+			<td><?=$content->body?></td>
+		</tr>
+		<?php endforeach ?>
+	</tbody>
+</table>
+</form>
