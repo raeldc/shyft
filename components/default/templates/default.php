079e3ff722945a1d0c8b21445bc4b929f4e27b02
Israel Canasa <raeldc@gmail.com>
2011-09-16 14:59:35 +0800

    When loading a template helper, pass the model state to the helper.

--- components/default/templates/default.php
+++ components/default/templates/default.php
@@ -69,4 +69,36 @@ class ComDefaultTemplateDefault extends KTemplateAbstract
 		
 		return $this;
 	}
+
+	/**
+     * Load a template helper
+     * 
+     * This function merges the elements of the attached view model state with the parameters passed to the helper
+     * so that the values of one are appended to the end of the previous one. 
+     * 
+     * If the view state have the same string keys, then the parameter value for that key will overwrite the state.
+     *
+     * @param   string  Name of the helper, dot separated including the helper function to call
+     * @param   mixed   Parameters to be passed to the helper
+     * @return  string  Helper output
+     */
+    public function renderHelper($identifier, $params = array())
+    {
+        $view = $this->getView();
+        
+        if(KInflector::isPlural($view->getName())) 
+        {
+            if($state = $view->getModel()->getState()) {
+                $params = array_merge( $state->getData(), $params);
+            }
+        } 
+        else 
+        {
+            if($item = $view->getModel()->getItem()) {
+                $params = array_merge( $item->getData(), $params);
+            }
+        }   
+        
+        return parent::renderHelper($identifier, $params);
+    }
 }
