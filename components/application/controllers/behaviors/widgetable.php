97a4b07ca8ef790eb1b942b4a527e2875b6c8b62
Israel Canasa <raeldc@gmail.com>
2011-09-18 12:13:34 +0800

    Improved widgetable behavior.
    Widgetable behavior assigns the container for the theme view.

--- components/application/controllers/behaviors/widgetable.php
+++ components/application/controllers/behaviors/widgetable.php
@@ -2,11 +2,27 @@
 
 class ComApplicationControllerBehaviorWidgetable extends KControllerBehaviorAbstract
 {
-    protected function _afterDispatch(KCommandContext $context)
+    public function __construct(KConfig $config)
     {
-        if ($context->application->isThemable()) 
+        parent::__construct($config);
+    
+        // Create the container identifier
+        $identifier = 'com://'.$this->_identifier->application.'/application.template.container';
+        KIdentifier::map('theme.container', $identifier);
+    }
+    
+    protected function _beforeDispatch(KCommandContext $context)
+    {
+        if ($context->caller->isThemable()) 
         {
+            // Make sure we inject the theme            
+            $config = array(
+                'view' => KFactory::get('theme'),
+            );
+
+            $container = KFactory::get('theme.container', $config);
             
+            KFactory::get('theme')->setContainer($container);
         }
     }
 }
