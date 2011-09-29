9d459a6cf5a1acc4b51dd2d07204d5aa166c4023
Israel Canasa <raeldc@gmail.com>
2011-09-29 18:04:45 +0800

    Added document object that uses "contents" collection only.

---
+++ components/content/databases/documents/contents.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @category	Shyft
+ * @package     Shyft_Database
+ * @subpackage  Table
+ * @copyright	Copyright (C) 2011 Israel Canasa. All rights reserved.
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link     	http://www.shyfted.com
+ */
+
+/**
+ * The table for all content types using the contents collection.
+ *
+ *
+ * @author      Israel Canasa <raeldc@gmail.com>
+ * @category    Shyft
+ * @package     Shyft_Database
+ * @subpackage  Document
+ */
+class ComContentDatabaseDocumentContents extends SDatabaseDocumentAbstract
+{
+	protected function _initialize(KConfig $config)
+	{
+		$config->name = 'contents';
+
+		parent::_initialize($config);
+	}
+}
