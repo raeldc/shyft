c57d75b6eeee4e89001c7da09bf44381eaa527dc
Israel Canasa <raeldc@gmail.com>
2011-10-02 13:29:07 +0800

    Just added examples of the how a Page will be created from content types.

--- components/pages/views/pages/tmpl/manage.php
+++ components/pages/views/pages/tmpl/manage.php
@@ -9,12 +9,29 @@
 	</div>
 	<div class="modal-body">
 		<h5>Content Types from Components</h5>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<ul class="text-grid">
+			<li>
+				<img src="media://application/generic-app.png" alt="Static Page">
+				<h5>Static Page</h5>
+				<p>Basic Static HTML</p>
+			</li>
+			<li>
+				<h5>Static Page</h5>
+				<p>Basic Static HTML</p>
+			</li>
+			<li>
+				<h5>Static Page</h5>
+				<p>Basic Static HTML</p>
+			</li>
+			<li>
+				<h5>Static Page</h5>
+				<p>Basic Static HTML</p>
+			</li>
+			<li>
+				<h5>Static Page</h5>
+				<p>Basic Static HTML</p>
+			</li>
+		</ul>
 
 		<h5>Content Types from Content Type Creator</h5>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
