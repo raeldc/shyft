4e2b9979e547a2e4e713208b5e99eca086b0cb06
Israel Canasa <raeldc@gmail.com>
2011-10-05 09:54:20 +0800

    Template changes. Remove list in pages view.
    We're going to use just the left side panel for listings of pages.

--- components/pages/views/pages/tmpl/default.php
+++ components/pages/views/pages/tmpl/default.php
@@ -2,25 +2,4 @@
     <?=@template('manage')?>
 </section>
 
-<form action="<?=@route()?>" method="get" class="-koowa-grid">
-<table class="zebra-striped" id="sortTableExample">
-	<thead>
-		<tr>
-			<th class="header">#</th>
-			<th class="yellow header">Title</th>
-			<th class="blue header">URL</th>
-		</tr>
-	</thead>
-
-	<tbody>
-		<?php foreach ($pages as $page): ?>
-		<tr>
-			<td><?=@helper('grid.checkbox',array('row' => $page))?></td>
-			<td><a href="<?=@route('view=page&id='.$page->id)?>&layout=form"><?=$page->title;?></a></td>
-			<td>index.php?page=<?=$page->permalink?></td>
-		</tr>
-		<?php endforeach ?>
-	</tbody>
-</table>
-<?= @helper('paginator.pagination', array('total' => $total)) ?>
-</form>
+This is the dashboard
--- components/pages/views/pages/tmpl/manage_tree.php
+++ components/pages/views/pages/tmpl/manage_tree.php
@@ -1,7 +1,7 @@
 <?php if ($total): ?>
 <ul>
 	<?php foreach ($pages as $page): ?>
-	<li><a href="<?=@route('view=page&id='.$page->id)?>"><?=$page->title;?></a></li>
+	<li><a href="<?=@route('layout=default&id='.$page->id)?>"><?=$page->title;?></a></li>
 	<?php endforeach ?>
 </ul>
 
