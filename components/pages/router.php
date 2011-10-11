d9131e9fa7f330cbddc8d0f70e34496dbce6d247
Israel Canasa <raeldc@gmail.com>
2011-10-11 18:28:07 +0800

    Added routers from staticpage and pages component.

--- components/pages/router.php
+++ components/pages/router.php
@@ -1,20 +1,24 @@
 <?php
 
-class ComStaticpageRouter extends ComDefaultRouter
+class ComPagesRouter extends ComDefaultRouter
 {
 	protected function _initialize(KConfig $config)
 	{
 		$config->append(array(
 			// The router will pass the route only if the first segment is its alias
-			'alias' => 'page',
+			'alias' => 'pages',
 
 			// string after : is a parameter
 			// string after # is the filter that will be used for the parameter. # is legal after :paramter e.g. :paramter#filter
 			// string inside [] is optional
 			'routes' => array(
-				'<id>'                          => 'view=staticpage&id=:id',
-				'pages[/page-<page>][/<limit>]' => 'view=staticpages&page=1&limit=10',
+				'manage/<id>[/<uri>]' => 'mode=manage&view=page&id=:id&layout=default',
 			),
+
+			// Regex that will be applied to check a parameter
+			'regex' => array(
+				'uri' => '.*'
+			)
 		));
 
 		parent::_initialize($config);
