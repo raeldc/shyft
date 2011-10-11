79fa831d428aef694e0f2ae40272103ca30743d5
Israel Canasa <raeldc@gmail.com>
2011-10-12 00:29:48 +0800

    Improved pages model. Get enabled items by default.

--- components/pages/models/pages.php
+++ components/pages/models/pages.php
@@ -4,30 +4,19 @@
  * Model to get a single page. 
  * This is used by the application dispatcher to get the current page.
  */
-class ComPagesModelPage extends SModelDefault
+class ComPagesModelPages extends SModelDefault
 {
 	public function __construct(KConfig $config)
 	{
 		parent::__construct($config);
 	
-		$this->_state->insert('page', 'cmd', null, true);
-	}
-
-	public function getList()
-	{
-		// In getting a list, we don't want to specify a page so we set it to null
-		$this->_state->page = null;
-
-		return parent::getList();
+		$this->_state->insert('enabled', 'cmd', true);
 	}
 
 	protected function _buildQueryWhere(SDatabaseQueryDocument $query)
 	{
-		if ($this->_state->page === 'default') {
-			$query->where('default', '=', true);
-		}
-		elseif (!empty($this->_state->page)) {
-			$query->where('permalink', '=', $this->_state->page);
+		if ($this->_state->enabled) {
+			$query->where('enabled', '=', true);
 		}
 	}
 }
