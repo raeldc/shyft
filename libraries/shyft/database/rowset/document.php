b1701d9289c469ac5e806f95fdac47a32238a8a7
Israel Canasa <raeldc@gmail.com>
2011-10-11 14:24:01 +0800

    Patch KDatabaseRowsetAbstract::find() to solve unfinished loop issue.

--- libraries/shyft/database/rowset/document.php
+++ libraries/shyft/database/rowset/document.php
@@ -2,5 +2,40 @@
 
 class SDatabaseRowsetDocument extends KDatabaseRowsetAbstract
 {
+	/**
+     * Returns a KDatabaseRow 
+     * 
+     * This functions accepts either a know position or associative array of key/value pairs
+     *
+     * @param   string|array  	The position or the key or an associatie array of column data 
+     *                          to match
+     * @return KDatabaseRow(set)Abstract Returns a row or rowset if successfull. Otherwise NULL.
+     */
+    public function find($needle)
+    {
+        $result = null;
+        
+        if(!is_scalar($needle))
+        {
+            $result = clone $this;
+            
+            foreach ($this as $i => $row) 
+            { 
+                foreach($needle as $key => $value)
+                {
+                    if(!in_array($row->{$key}, (array) $value)) {
+                        $result->extract($row);
+                    } 
+                }
+            }
+        }
+        else 
+        {
+            if(isset($this->_object_set[$needle])) {
+                $result = $this->_object_set[$needle];
+            }
+        }
 
+        return $result;
+    }
 }
