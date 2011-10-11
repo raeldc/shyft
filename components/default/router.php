cfc5fa1e6169520ba77fb67205b55472e3495166
Israel Canasa <raeldc@gmail.com>
2011-10-11 16:03:18 +0800

    Removed the default routes in the ComDefaultRouter

--- components/default/router.php
+++ components/default/router.php
@@ -5,11 +5,8 @@ class ComDefaultRouter extends SRouterDefault
 	protected function _initialize(KConfig $config)
 	{
 		$config->append(array(
-			'routes' => array(
-				'<view>'          => 'view=<view>',
-				'<view>/new'      => 'view=<view>&layout=form',
-				'<view>/edit/<id>' => 'view=<view>&layout=form&id=<id>',
-			),
+			'alias'  => $this->getIdentifier()->package,
+			'routes' => array(),
 		));
 	
 		parent::_initialize($config);
