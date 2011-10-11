cb53d9872f4e672e9dcae706854f4a0451892e0b
Israel Canasa <raeldc@gmail.com>
2011-10-11 12:12:42 +0800

    In routable behavior, add detection of URI.

--- components/application/controllers/behaviors/routable.php
+++ components/application/controllers/behaviors/routable.php
@@ -7,7 +7,7 @@ class ComApplicationControllerBehaviorRoutable extends KControllerBehaviorAbstra
 		$config->append(array(
 			'priority' => KCommand::PRIORITY_HIGHEST,
 		));
-	
+
 		parent::_initialize($config);
 	}
 	
@@ -29,4 +29,73 @@ class ComApplicationControllerBehaviorRoutable extends KControllerBehaviorAbstra
 		$context->caller->setRequest($page->parameters);
 		$context->caller->setComponent($component);
     }
+
+    /**
+	 * Get the URI of the request using PATH_INFO,
+	 * REQUEST_URI, PHP_SELF or REDIRECT_URL.
+	 *
+	 * @return  string  URI of the request
+	 * @throws  KDispatcherException
+	 */
+	protected static function _getURI()
+	{
+		if (!empty($_SERVER['PATH_INFO']))
+		{
+			// PATH_INFO does not contain the docroot or index
+			$uri = $_SERVER['PATH_INFO'];
+		}
+		else
+		{
+			// REQUEST_URI and PHP_SELF include the docroot and index
+
+			if(isset($_SERVER['REQUEST_URI']))
+			{
+				/**
+				 * We use REQUEST_URI as the fallback value. The reason
+				 * for this is we might have a malformed URL such as:
+				 *
+				 *  http://localhost/http://example.com/judge.php
+				 *
+				 * which parse_url can't handle. So rather than leave empty
+				 * handed, we'll use this.
+				 */
+				$uri = $_SERVER['REQUEST_URI'];
+
+				if ($request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))
+				{
+					// Valid URL path found, set it.
+					$uri = $request_uri;
+				}
+
+				// Decode the request URI
+				$uri = rawurldecode($uri);
+			}
+			elseif(isset($_SERVER['PHP_SELF']))
+			{
+				$uri = $_SERVER['PHP_SELF'];
+			}
+			elseif(isset($_SERVER['REDIRECT_URL']))
+			{
+				$uri = $_SERVER['REDIRECT_URL'];
+			}
+			else
+			{
+				throw new KDispatcherException('Unable to get the URI using PATH_INFO, REQUEST_URI, PHP_SELF or REDIRECT_URL');
+			}
+
+			// Get the path from the base URL, including the index file
+			$base_url = parse_url(KRequest::base(), PHP_URL_PATH);
+
+			if(strpos($uri, $base_url) === 0)
+			{
+				// Remove the base URL from the URI
+				$uri = (string) substr($uri, strlen($base_url));
+			}
+
+			// Remove the index file from the URI
+				$uri = (string) substr($uri, strlen('index.php'));
+		}
+
+		return $uri;
+	}
 }
