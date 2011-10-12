6885070705cc1ed3650412f24e7b29e70de83658
Israel Canasa <raeldc@gmail.com>
2011-10-12 18:46:49 +0800

    Make SRouterDefault a singleton

--- libraries/shyft/router/default.php
+++ libraries/shyft/router/default.php
@@ -1,6 +1,25 @@
 <?php
 
-class SRouterDefault extends SRouter
+class SRouterDefault extends SRouter implements KServiceInstantiatable
 {
-	
+	/**
+     * Force creation of a singleton
+     *
+     * @param 	object 	An optional KConfig object with configuration options
+     * @param 	object	A KServiceInterface object
+     * @return  SRouterDefault
+     */
+    public static function getInstance(KConfigInterface $config, KServiceInterface $container)
+    {
+        // Check if an instance with this identifier already exists or not
+        if (!$container->has($config->service_identifier))
+        {
+            //Create the singleton
+            $classname = $config->service_identifier->classname;
+            $instance  = new $classname($config);
+            $container->set($config->service_identifier, $instance);
+        }
+        
+        return $container->get($config->service_identifier);
+    }
 }
