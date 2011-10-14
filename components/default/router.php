9c4777de614fb034a7aec44c06ac5c2aa786075a
Israel Canasa <raeldc@gmail.com>
2011-10-14 13:33:31 +0800

    ComDefaultRouter sets a default route for view and layout parameters.

--- components/default/router.php
+++ components/default/router.php
@@ -2,5 +2,15 @@
 
 class ComDefaultRouter extends SRouterDefault
 {
+	protected function _initialize(KConfig $config)
+	{
+		$config->append(array(
+			'routes' => array(
+				'[<view>[/<layout>]]' => 'view=!&layout',
+			),
+		));
+	
+		parent::_initialize($config);
+	}
 	
 }
