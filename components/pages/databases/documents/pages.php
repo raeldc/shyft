08b8ee2ebf65eabf7a45939d98f129ddde27b39a
Israel Canasa <raeldc@gmail.com>
2011-10-16 22:12:30 +0800

    Removed experimental code

--- components/pages/databases/documents/pages.php
+++ components/pages/databases/documents/pages.php
@@ -2,12 +2,5 @@
 
 class ComPagesDatabaseDocumentPages extends SDatabaseDocumentDefault
 {
-	protected function _initialize(KConfig $config)
-	{
-		$config->append(array(
-			'behaviors' => array('relatable'),
-		));
 	
-		parent::_initialize($config);
-	}
 }
