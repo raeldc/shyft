0990f62eff3d10b0b0dd3fb8107a7f7202869fc5
Israel Canasa <raeldc@gmail.com>
2011-10-16 17:49:44 +0800

    No need to issue a query in the node model. This will be done by the pageable document behavior.

--- components/default/models/node.php
+++ components/default/models/node.php
@@ -2,19 +2,5 @@
 
 class ComDefaultModelNode extends SModelDocument
 {
-	public function __construct(KConfig $config)
-	{
-		parent::__construct($config);
 	
-		$this->_state->insert('page', 'cmd', '');
-	}
-
-	protected function _buildQueryWhere(SDatabaseQueryDocument $query)
-	{
-		parent::_buildQueryWhere($query);
-
-		if (!empty($this->_state->page)) {
-			$query->where('page', '=', $this->_state->page);
-		}
-	}
 }
