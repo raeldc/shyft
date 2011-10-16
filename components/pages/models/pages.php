733880ad7cfa74087031533259b09ba81450f3c6
Israel Canasa <raeldc@gmail.com>
2011-10-16 11:44:19 +0800

    Make the pages model a singleton.

--- components/pages/models/pages.php
+++ components/pages/models/pages.php
@@ -4,16 +4,37 @@
  * Model to get a single page. 
  * This is used by the application dispatcher to get the current page.
  */
-class ComPagesModelPages extends SModelDefault
+class ComPagesModelPages extends SModelDefault implements KServiceInstantiatable
 {
 	public function __construct(KConfig $config)
 	{
 		parent::__construct($config);
 	
+		$this->_state = new KConfigState();
+
 		$this->_state
 			->insert('enabled', 'boolean', false)
 			->insert('page', 'cmd', '', true);
 	}
+
+	/**
+     * Force creation of a singleton
+     *
+     * @return ComPagesModelPages
+     */
+    public static function getInstance(KConfigInterface $config, KServiceInterface $container)
+    {
+        // Check if an instance with this identifier already exists or not
+        if (!$container->has($config->service_identifier))
+        {
+            //Create the singleton
+            $classname = $config->service_identifier->classname;
+            $instance  = new $classname($config);
+            $container->set($config->service_identifier, $instance);
+        }
+        
+        return $container->get($config->service_identifier);
+    }
 
 	protected function _buildQueryWhere(SDatabaseQueryDocument $query)
 	{
