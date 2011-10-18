e4a62a522b3fef5ee795cd88e8f913e04e07478b
Israel Canasa <raeldc@gmail.com>
2011-10-18 17:43:45 +0800

    Renamed pages_pages collection to pages.

--- components/pages/databases/documents/pages.php
+++ components/pages/databases/documents/pages.php
@@ -5,7 +5,8 @@ class ComPagesDatabaseDocumentPages extends SDatabaseDocumentDefault
 	protected function _initialize(KConfig $config)
 	{
 		$config->identity_column = 'slug';
-	
+		$config->name = 'pages';
+		
 		parent::_initialize($config);
 	}
 }
