c8188a317cc71acff423cb39106262e8f78683cd
Israel Canasa <raeldc@gmail.com>
2011-10-21 12:21:41 +0800

    Assign the name of widgets collection as "widgets" instead of widgets_widgets

--- components/widgets/databases/documents/widgets.php
+++ components/widgets/databases/documents/widgets.php
@@ -2,5 +2,11 @@
 
 class ComWidgetsDatabaseDocumentWidgets extends SDatabaseDocumentDefault
 {
-
+	protected function _initialize(KConfig $config)
+	{
+		$config->name = 'widgets';
+	
+		parent::_initialize($config);
+	}
+	
 }
