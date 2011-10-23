0d487ea25181d94fad605c028b22bb70fd07bb7c
Israel Canasa <raeldc@gmail.com>
2011-10-23 23:09:36 +0800

    Added group route rules in the pages router.

--- components/pages/router.php
+++ components/pages/router.php
@@ -6,9 +6,11 @@ class ComPagesRouter extends ComDefaultRouter
 	{
 		$config->append(array(
 			'routes' => array(
-				'new'       => 'view=page&layout=#form',
-				'edit/<id>' => 'view=page&layout=#form&id=!',
-				'<layout>'  => 'view=#pages&layout=default',
+				'new'             => 'view=page&layout=#form',
+				'edit/<id>'       => 'view=page&layout=#form&id=!',
+				'group/new'       => 'view=#group&layout=#form',
+				'group/edit/<id>' => 'view=#group&layout=#form&id=!',
+				'<layout>'        => 'view=#pages&layout=default',
 			),
 		));
 
