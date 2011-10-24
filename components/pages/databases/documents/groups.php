7623f6b9884aae73b4c3f60196fd6e2978399267
Israel Canasa <raeldc@gmail.com>
2011-10-24 16:12:57 +0800

    Groups document object now does not use sluggable behavior. Simply use ID instead.

--- components/pages/databases/documents/groups.php
+++ components/pages/databases/documents/groups.php
@@ -4,14 +4,7 @@ class ComPagesDatabaseDocumentGroups extends SDatabaseDocumentDefault
 {
 	protected function _initialize(KConfig $config)
 	{
-		$config->identity_column = 'slug';
 		$config->name = 'pages_groups';
-
-		$sluggable = $this->getBehavior('sluggable', array(
-			'columns' => array('name')
-		));
-
-		$config->behaviors = array($sluggable);
 
 		parent::_initialize($config);
 	}
