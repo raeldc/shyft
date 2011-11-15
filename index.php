9566529305285733dc0f585c4f01f1a70747cb31
Israel Canasa <raeldc@gmail.com>
2011-11-15 21:39:34 +0800

    Removed Widgets and Actions loader adapter and service locator.
    We don't need them now. We'll add them back if the need arises. But for now they are useless.

--- index.php
+++ index.php
@@ -6,10 +6,7 @@
 define('SYSTEM_ROOT',       realpath(dirname(__FILE__)));
 define('SYSTEM_LIBRARIES',  realpath(SYSTEM_ROOT.'/libraries'));
 define('SYSTEM_COMPONENTS', realpath(SYSTEM_ROOT.'/components'));
-define('SYSTEM_WIDGETS',    realpath(SYSTEM_ROOT.'/widgets'));
-define('SYSTEM_ACTIONS',    realpath(SYSTEM_ROOT.'/actions'));
 define('SYSTEM_THEMES',     realpath(SYSTEM_ROOT.'/themes'));
-define('SYSTEM_LANGUAGES',  realpath(SYSTEM_ROOT.'/languages'));
 
 /*********************************************
  *  Define various constants for all sites   *
@@ -18,10 +15,7 @@ define('SYSTEM_LANGUAGES',  realpath(SYSTEM_ROOT.'/languages'));
 define('SITES_ROOT',        SYSTEM_ROOT.'/sites/all');
 define('SITES_LIBRARIES',   SITES_ROOT.'/libraries');
 define('SITES_COMPONENTS',  SITES_ROOT.'/components');
-define('SITES_WIDGETS',     SITES_ROOT.'/widgets');
-define('SITES_ACTIONS',     SITES_ROOT.'/actions');
 define('SITES_THEMES',      SITES_ROOT.'/themes');
-define('SITES_LANGUAGES',   SITES_ROOT.'/languages');
 
 /************************************************************************************************
  *                  Automatically detect the site resource that will be used.                   *
@@ -37,10 +31,7 @@ $site = (is_dir($site='sites/'.$_SERVER['HTTP_HOST'])) ? $site: 'sites/default';
 define('SITE_ROOT',         realpath(SYSTEM_ROOT.'/'.$site));
 define('SITE_LIBRARIES',    SITE_ROOT.'/libraries');
 define('SITE_COMPONENTS',   SITE_ROOT.'/components');
-define('SITE_WIDGETS',      SITE_ROOT.'/widgets');
-define('SITE_ACTIONS',      SITE_ROOT.'/actions');
 define('SITE_THEMES',       SITE_ROOT.'/themes');
-define('SITE_LANGUAGES',    SITE_ROOT.'/languages');
 
 /************************************************************************************************
  *                               Initialize Shyft Subframework                                   *
@@ -54,10 +45,7 @@ Shyft::getInstance(array(
     // Shyft will look for files in those folders, starting on the first path, until the file is found.
     'libraries'  => array(SITE_LIBRARIES,  SITES_LIBRARIES,    SYSTEM_LIBRARIES),
     'components' => array(SITE_COMPONENTS, SITES_COMPONENTS,   SYSTEM_COMPONENTS),
-    'widgets'    => array(SITE_WIDGETS,    SITES_WIDGETS,      SYSTEM_WIDGETS),
-    'actions'    => array(SITE_ACTIONS,    SITES_ACTIONS,      SYSTEM_ACTIONS),
     'themes'     => array(SITE_THEMES,     SITES_THEMES,       SYSTEM_THEMES),
-    'languages'  => array(SITE_LANGUAGES,  SITES_LANGUAGES,    SYSTEM_LANGUAGES),
     'site'       => array(SITE_ROOT,       SITES_ROOT,         SYSTEM_ROOT),
 ));
 
