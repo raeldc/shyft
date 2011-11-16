bb049c9903011221ee0056d67bbe5197799e66ed
Israel Canasa <raeldc@gmail.com>
2011-11-17 00:12:47 +0800

    In page form, added toolbar instead of using custom buttons.
    Also use $page->id instead of $page->slug.

--- components/pages/views/page/tmpl/form.php
+++ components/pages/views/page/tmpl/form.php
@@ -1,3 +1,7 @@
+<section class="widget replace toolbar-top">
+    <?= @helper('toolbar.render', array('toolbar' => $toolbar))?>
+</section>
+
 <form action="<?=@route('id='.$page->id)?>" method="post" class="-koowa-form">
 <fieldset>
     <legend>Create Page</legend>
@@ -13,7 +17,7 @@
         <label for="form-slug">Slug</label>
         <div class="input input-prepend">
             <span class="add-on"><?=KRequest::base()?>/</span>
-            <input id="form-slug" name="slug" size="5" type="text" value="<?=$page->slug?>">
+            <input id="form-slug" name="slug" size="5" type="text" value="<?=$page->id?>">
         </div>
     </div>
 
@@ -38,20 +42,6 @@
     <fieldset>
         <ul class="tabs" data-tabs="tabs">
             <li class="active"><a href="#page-content">Type</a></li>
-            <li><a href="#page-component">Options</a></li>
-            <li><a href="#page-layout">Layout</a></li>
-            <li><a href="#page-widgets">Widgets</a></li>
-            <li><a href="#page-display">User Access</a></li>
-            <li class="dropdown" data-dropdown="dropdown">
-                <a href="#" class="dropdown-toggle">More Options</a>
-                <ul class="dropdown-menu">
-                    <li><a href="#page-search">SEO</a></li>
-                    <li><a href="#">Redirections</a></li>
-                    <li><a href="#page-revisions">Revision History</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">Help</a></li>
-                </ul>
-            </li>
         </ul>
 
         <div class="tab-content">
@@ -60,87 +50,12 @@
                 <div class="clearfix">
                     <label for="form-position">Content Type</label>
                     <div class="input">
-                        <select class="medium" name="type">
-                            <option value="com://site/static">Static HTML Page</option>
-                            <option value="com://site/pages">Pages</option>
-                            <option value="com://site/widgets">Widgets</option>
-                            <option value="com://site/content">Contents</option>
-                        </select>
-                        <span class="help-block">
-                            You can point this page to a Shyft Component.
-                        </span>
+                        <?=@service('com://site/content.template.helper.listbox')->types(array(
+                            'selected' => $page->type
+                        ))?>
                     </div>
                 </div>
              </div><!--end: #page-content-->
-
-             <!--start: #page-component-->
-            <div id="page-component">
-                <div class="clearfix">
-                    <label for="form-component">Page Title</label>
-                    <div class="input">
-                        <input class="xlarge" id="form-component" name="component[title]" size="30" type="text" value="<?=$component->title?>">
-                    </div>
-                </div>
-
-                <div class="clearfix">
-                <label for="form-body">Body</label>
-                <div class="input">
-                    <textarea class="xlarge" id="form-body" name="body" rows="5"><?=$component->body?></textarea>
-                </div>
-            </div>
-             </div><!--end: #page-component-->
-
-             <!--start: #page-search-->
-            <div id="page-search">
-                <h4>Search Engine Optimization</h4>
-                <div class="clearfix">
-                    <label>SEO Options</label>
-
-                    <ul class="input inputs-list">
-                        <li>
-                            <label>
-                                <input type="radio" checked="" name="force_meta" value="0">
-                                <span>Allow components to set values, these are just defaults</span>
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type="radio" name="force_meta" value="1">
-                                <span>Forcefully use these values, ignoring the component&rsquo;s values</span>
-                            </label>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="clearfix">
-                    <label for="form-page_title">Page Title</label>
-                    <div class="input input-prepend">
-                        <label class="add-on"><input type="checkbox" name="enabled[page_title]" value="1"></label>
-                        <input class="large" id="form-page_title" name="page_title" size="16" type="text">
-                        <span class="help-block">Browser window title</span>
-                    </div>
-                </div>
-
-                <div class="clearfix">
-                    <label for="form-page_title">Meta Description</label>
-                    <div class="input input-prepend">
-                        <label class="add-on"><input type="checkbox" name="enabled[meta_description]" value="1"></label>
-                        <input class="large" id="form-meta_description" name="meta_description" size="16" type="text">
-                    </div>
-                </div>
-
-                <div class="clearfix">
-                    <label for="form-meta_keywords">Meta Keywords</label>
-                    <div class="input input-prepend">
-                        <label class="add-on">
-                            <input type="checkbox" name="enabled[meta_keywords]" value="1">
-                        </label>
-
-                        <input class="xlarge" id="form-meta_keywords" name="meta_keywords" size="16" type="text">
-                    </div>
-                </div>
-
-            </div><!--end: #page-search-->
 
              <!--start: #page-display-->
             <div id="page-display">
@@ -149,8 +64,5 @@
 
         </div>
     </fieldset>
-    <div class="actions">
-        <input type="submit" class="btn primary" value="Save Changes">&nbsp;<button type="reset" class="btn">Cancel</button>
-    </div>
 </fieldset>
 </form>
