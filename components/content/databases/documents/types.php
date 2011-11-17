695b4f9794de70e417d3df766e7e0ca696df61b6
Israel Canasa <raeldc@gmail.com>
2011-11-17 13:09:25 +0800

    Use the real id of a content type document instead of its slug. The real id is immutable.

--- components/content/databases/documents/types.php
+++ components/content/databases/documents/types.php
@@ -4,9 +4,8 @@ class ComContentDatabaseDocumentTypes extends SDatabaseDocumentDefault
 {
 	protected function _initialize(KConfig $config)
 	{
-		$config->identity_column = 'type';
 		$config->name = 'content_types';
-		
+
 		parent::_initialize($config);
 	}
 }
