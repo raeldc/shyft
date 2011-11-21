633d5b5424355a152b2df82d781c971a5b6ded4d
Israel Canasa <raeldc@gmail.com>
2011-11-21 17:31:30 +0800

    On the widget row, on getter, if "type" is being retrieved, automatically load the content type based on the id.

---
+++ components/widgets/databases/rows/widget.php
@@ -0,0 +1,29 @@
+<?php
+
+class ComWidgetsDatabaseRowWidget extends SDatabaseRowDefault
+{
+	/**
+     * Get a value by key, returning a fetched data if it's a reference
+     *
+     * @param   string  The key name
+     */
+	public function __get($key)
+	{
+		if ($key == 'type') 
+		{
+			$result = parent::__get($key);
+
+			$type          = clone $this->getIdentifier();
+			$type->package = 'content';
+			$type->name    = 'types';
+			$type          = $this->getService($type);
+
+			if ($result) {
+				$type->setData(array('id' => $result))->load();
+			}
+
+			return $type;
+		}
+		else return parent::__get($key);
+	}
+}
