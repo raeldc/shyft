b66c814b0d9d9ddcf949f4e11728811730c1833e
Israel Canasa <raeldc@gmail.com>
2011-11-22 18:14:20 +0800

    Added container listbox template helper to list all containers of a theme.

---
+++ components/themes/templates/helpers/listbox.php
@@ -0,0 +1,50 @@
+<?php
+
+class ComThemesTemplateHelperListbox extends KTemplateHelperListbox
+{
+	public function containers($config = array())
+	{
+		$config = new KConfig($config);
+		$config->append(array(
+			'name'		  => 'name',
+			'attribs'	  => array(),
+			'model'		  => 'themes',
+		    'prompt'      => '- Select -', 
+		    'unique'	  => true
+		))->append(array(
+			'value'		 => $config->name,
+			'selected'   => $config->{$config->name},
+		    'identifier' => 'com://'.$this->getIdentifier()->application.'/'.$this->getIdentifier()->package.'.model.'.KInflector::pluralize($config->model)
+		))->append(array(
+			'text'		=> 'title',
+			'column'    => $config->value,
+			'deselect'  => false,
+		))->append(array(
+		    'filter' 	=> array('sort' => $config->text),
+		));
+
+		$themes = $this->getService($config->identifier)->set($config->filter)->getList();
+
+		//Compose the options array
+        $options   = array();
+
+ 		if($config->deselect) {
+         	$options[] = $this->option(array('text' => $config->prompt));
+        }
+		
+ 		foreach($themes as $theme) 
+ 		{
+ 			foreach ($theme->layouts as $layout) 
+ 			{
+ 				foreach ($layout->containers as $item) 
+ 				{
+ 		    		$options[] =  $this->option(array('text' => $item->{$config->text}.' ('.$theme->name.'/'.$layout->name.')', 'value' => $item->{$config->value}));		
+ 				}
+ 			}
+		}
+
+		//Add the options to the config object
+		$config->options = $options;
+		return $this->optionlist($config);
+	}
+}
