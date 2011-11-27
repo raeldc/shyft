1ce9821472667c9fe121bba9e4e63f8b5e7b5da7
Israel Canasa <raeldc@gmail.com>
2011-11-27 16:29:24 +0800

    Widgets: Added Template for a widget type "Content List"

---
+++ components/content/views/config/tmpl/list.php
@@ -0,0 +1,6 @@
+<div class="clearfix">
+    <label>Item Count</label>
+    <div class="input">
+        <input class="small" name="params[item_count]" size="2" type="text" value="<?=$widget->params->item_count ?>">
+    </div>
+</div>
